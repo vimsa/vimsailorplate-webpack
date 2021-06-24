6f8de1956495d9db7041474d761ab833b465b5a3
Surya Aditya <vimsa.dev@gmail.com>
2021-06-24 23:07:29 +0800

    🍱 Initialize

---
+++ README.md
@@ -0,0 +1,14 @@
+# VIMSA Boilerplate
+
+## Setup
+
+Download [Node.js](https://nodejs.org/en/download/).
+Run this followed commands:
+
+```bash
+# Install dependencies (only for first time)
+npm i
+
+# Serve
+npm start
+```
--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express')
+const express = require("express")
 const app = express()
+const path = require("path")
 const port = 3000
 
-const path = require('path')
 
-app.set('views', path.join(__dirname, 'src/views'))
+app.set("views", path.join(__dirname, 'src/views'))
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-   res.render('index', {
+app.get("/", (req, res) => {
+   res.render("pages/home", {
       meta: {
          title: "VIMSΛ",
          description: "Surya Aditya - Creative developer"
@@ -16,6 +16,25 @@ app.get('/', (req, res) => {
    })
 })
 
+app.get("/about", (req, res) => {
+   res.render("pages/about", {
+      meta: {
+         title: "VIMSΛ",
+         description: "Surya Aditya - Creative developer"
+      }
+   })
+})
+
+app.get("/work", (req, res) => {
+   res.render("pages/work", {
+      meta: {
+         title: "VIMSΛ",
+         description: "Surya Aditya - Creative developer"
+      }
+   })
+})
+
+
 app.listen(port, () => {
-   console.log(`Vimsa http://localhost:${port}`)
+   console.log(`VIMSΛ App listening at http://localhost:${port}`)
 })
